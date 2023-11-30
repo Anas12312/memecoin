4d0046065cfd62602c58059cc21f51ed93a454e6
anas12312 <anashesham000@gmail.com>
2023-12-01 00:12:13 +0200

    anas

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Roadmap() {
   return (
-    <div className='w-full overflow-hidden pt-20 relative'>
-        
+    <div className='w-full overflow-hidden pt-20 relative flex justify-center items-center'>
+        <div className='w-[80%] h-full flex'>
+            <div>
+                <img src="https://static.wixstatic.com/media/1f3f2b_33505209bd5340c581cc5e861ff74845~mv2.png/v1/crop/x_455,y_0,w_2691,h_2700/fill/w_370,h_371,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/pepepissingnobackground%20copy.png" alt="" />
+            </div>
+            <div></div>
+            <div>
+                <img src="https://static.wixstatic.com/media/1f3f2b_21fe381c89284e328827e6c35f4b5513~mv2.png/v1/fill/w_366,h_371,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Untitled%20design%20-%202023-03-30T220301_142.pngs" alt="" />
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Tokenomics() {
   return (
-    <div className='w-full overflow-hidden pt-20 relative'>
+    <div>
         
     </div>
   )
