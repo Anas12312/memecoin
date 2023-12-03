e6437339d0fbdf7b5c6b213c8fbdb5e99f9e556d
Zyad Abdul-Nasser <zizo_007@live.com>
2023-12-04 01:16:48 +0200

    z

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
         <div className='w-full overflow-hidden pb-20 relative border-b-4 border-white h-[40rem]' ref={myRef}>
             <img className='object-cover opacity-20' src="" alt="" />
             <div className='w-full h-full absolute top-0 flex flex-col justify-center items-center'>
-                <div className='h-[55%] text-white w-[60%] flex justify-between items-center'>
+                <div className='h-[55%] text-white w-[60%] flex justify-between items-center blur-bg p-4 border-2 border-black rounded-lg'>
                     <div className='h-full w-[80%] flex justify-start items-start flex-col'>
                         <div className='p-5 text-6xl font-Next-Bravo outline-text text-gradiant'>
                             <h1>$Harold</h1>
--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -3,28 +3,32 @@ import React from 'react'
 export default function Tokenomics() {
   return (
     <div className='w-full overflow-hidden pt-14 pb-32 relative bg-green flex flex-col items-center border-y-4 border-white'>
-      <div className='text-8xl text-white font-Urban-Melted outline-text text-gradiant'>
-        TOKENOMICS
-      </div>
+      <div className='flex flex-col items-center blur-bg p-8 border-2 border-black rounded-lg'>
 
-      <div className='flex flex-col justify-center mt-16 items-center text-4xl text-white font-Chewy '>
-        <div>token supply:</div>
-        <div className='text-4xl'>690,000,000,000,000</div>
-      </div>
+        <div className='text-8xl text-white font-Urban-Melted outline-text text-gradiant'>
+          TOKENOMICS
+        </div>
 
-      <div className="bg-gradient-to-r from-purple-500 to-indigo-500 p-6 rounded-tr-3xl rounded-bl-3xl shadow-lg
+        <div className='flex flex-col justify-center mt-16 items-center text-4xl text-white font-Chewy '>
+          <div>token supply:</div>
+          <div className='text-4xl'>690,000,000,000,000</div>
+        </div>
+
+        <div className="bg-gradient-to-r from-purple-500 to-indigo-500 p-6 rounded-tr-3xl rounded-bl-3xl shadow-lg
                       text-white mt-10 text-4xl border-[#274f79]">
-        <div>No Taxes, No Bullshit. It’s that simple. </div>
-        <br></br>
-        <div>LP tokens are burnt, and contract ownership is renounced. </div>
-      </div>
-      {/* <div className='flex flex-col justify-center items-start text-4xl shadow-[0px_0px_30px_-10px_#FFFFFF] 
+          <div>No Taxes, No Bullshit. It’s that simple. </div>
+          <br></br>
+          <div>LP tokens are burnt, and contract ownership is renounced. </div>
+        </div>
+        {/* <div className='flex flex-col justify-center items-start text-4xl shadow-[0px_0px_30px_-10px_#FFFFFF] 
                               border-2 w-2/4 px-7 py-5 text-white mt-10 bg-[#274f79] rounded-tr-3xl rounded-bl-3xl'>
         <div>No Taxes, No Bullshit. It’s that simple. </div>
         <br></br>
         <div>LP tokens are burnt, and contract ownership is renounced. </div>
       </div> */}
-      
+
+      </div>
+
     </div>
   )
 }
