1c53ae652b5d2cce84f2e5a4195ab9dc5887be77
anas12312 <anashesham000@gmail.com>
2023-12-29 21:38:54 +0200

    anas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar({
     e.target?.classList.remove('text-gradiant')
   }
   const comingSoon = () => {
-    alert('Coming Soon...')
+    window.open('https://jup.ag/swap/USDC-harold_NcuYZr46WAwAMTqXT5XfiXTmagDR5gyurYK4hsADFVz', '_blank');
   }
   return (
     <div className='nav-bar z-10 '>
@@ -26,7 +26,7 @@ export default function Navbar({
         <div className='p-5 text-3xl flex justify-start items-center'>
           <div className='mx-2 hover:scale-110 trans hover:cursor-pointer w-12'
                 onClick={() => {
-                  window.open('https://jup.ag/swap/USDC-harold_NcuYZr46WAwAMTqXT5XfiXTmagDR5gyurYK4hsADFVz', '_blank');
+                  window.open('https://twitter.com/HaroldonSol', '_blank');
                 }}>
             <img src="https://static.wixstatic.com/media/1f3f2b_f59a557a4c72456581fa206a9247dcac~mv2.png/v1/fill/w_106,h_106,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/twtttt.png" alt="" />
           </div>
